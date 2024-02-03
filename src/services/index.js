53b7b654ae00f3ebc08023fd092ea08d5aea85b3
qglove <2545671842@qq.com>
2024-02-03 14:22:48 +0800

    feat

--- src/services/index.js
+++ src/services/index.js
@@ -4,10 +4,10 @@ import { store } from 'qg-react-components';
 import { onClearLoginOutTime, setLoading } from '@/utils/pubilc';
 
 export const config = {
-  baseURL: 'http://103.117.121.53/',
+  baseURL: 'http://1.2.3.4/',
   wsList: {
-    docker: '192.168.1.102:8881/',
-    api: '103.117.121.53/',
+    docker: '1.2.3.4/',
+    api: '1.2.3.4/',
   },
   timeout: 15000,
 };
